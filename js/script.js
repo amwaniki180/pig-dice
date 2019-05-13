83ddbd40e8ccb8ed7824e2a079bfd94596c2fcc8
Antony <amwaniki180@gmail.com>
2019-05-13 06:37:08 +0300

    randomnization

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ var tally = [0,0]
 var total = 0;
 var currentPlayer = 0;
 var active = true;
+$(document).ready(function(){
+  document.querySelector('.player1').classlist.add('active')
+  var rollDice = function() {
+
+  if (active) {
+      var randomNum = Math.floor(Math.random() * 6 + 1);
+      document.getElementById('rolled' + currentPlayer).innerHTML = "you rolled:" + randomNum;
+
+      if (randomNum == 1) {
+         $("#output" + currentPlayer).show();
+          nextPlayer();
+       }
+}
