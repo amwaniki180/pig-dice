c9aa4fd4900b159a43946add6e21b117f7ab6372
Antony <amwaniki180@gmail.com>
2019-05-13 14:06:04 +0300

    beautified js,html and css

--- js/script.js
+++ js/script.js
@@ -72,40 +72,40 @@
 
 // });
 class Player {
-    constructor(score, total) {
-        this.score = score;
-        this.total = total;
-    }
+  constructor(score, total) {
+    this.score = score;
+    this.total = total;
+  }
 }
 var player1 = new Player(0, 0);
 var player2 = new Player(0, 0);
 $(document).ready(function() {
-    $("#rollp1").click(function() {
-        $(".round2").hide()
-        player1.score = Math.floor((Math.random() * 6) + 1);
-        $("#result1").text(player1.score);
-        player1.total += player1.score;
-        $("#total1").text(player1.total);
-        if (player1.score === 1) {
-            alert("SORRY....NEXT PLAYER")
-            $(".round2").show()
-        }
-    })
-    $("#rollp2").click(function() {
-        $(".round1").hide()
-        player2.score = Math.floor((Math.random() * 6) + 1);
-        $("#result2").text(player2.score);
-        player2.total += player2.score;
-        $("#total2").text(player2.total);
-        if (player1.score === 1) {
-            alert("SORRY....NEXT PLAYER")
-            $(".round1").show()
-        }
-    })
-    $("#holdp1").click(function() {
-        $(".round2").hide()
-    })
-    $("#holdp2").click(function() {
-        $(".round1").hide()
-    })
+  $("#rollp1").click(function() {
+    $(".round2").hide()
+    player1.score = Math.floor((Math.random() * 6) + 1);
+    $("#result1").text(player1.score);
+    player1.total += player1.score;
+    $("#total1").text(player1.total);
+    if (player1.score === 1) {
+      alert("SORRY....NEXT PLAYER")
+      $(".round2").show()
+    }
+  })
+  $("#rollp2").click(function() {
+    $(".round1").hide()
+    player2.score = Math.floor((Math.random() * 6) + 1);
+    $("#result2").text(player2.score);
+    player2.total += player2.score;
+    $("#total2").text(player2.total);
+    if (player1.score === 1) {
+      alert("SORRY....NEXT PLAYER")
+      $(".round1").show()
+    }
+  })
+  $("#holdp1").click(function() {
+    $(".round2").hide()
+  })
+  $("#holdp2").click(function() {
+    $(".round1").hide()
+  })
 })
